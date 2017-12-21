8e5b872aed05311c9bec8b61485d9aee81a5da4d
Blake Wetherton <bwetherton@wisc.edu>
2017-12-20 23:09:02 -0600

    Comments

--- solveorbit.c
+++ solveorbit.c
@@ -13,8 +13,10 @@
 
 #define USEJAC 0
 #define DIM 6
-#define TILEX 20
+#define TILEX 20 //Eventually planned to be used to manage memory for interp grid
 #define TILEZ 20
+
+//globals allow fewer arguments to be passed.
 
 const char jobname[128], outdir[256],gdadir[256];
 int Nvx, Nvy, Nvz;
@@ -29,11 +31,11 @@ int main(int argc,char *argv[]){
   int rank, numprocs;
   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  initialize();
-  omp_set_num_threads(nthreads);
+  initialize(); //reads "orbit.cfg" and sets the globals
+  omp_set_num_threads(nthreads); //for scaling characterization
   fieldgrid masterfield;
-  if(slice==0)
-    masterfield = loadfieldsPeter(nx,nz,Lx,Lz,fileext,gdadir);
+  if(slice==0) //Slice = 0 implies gdas are made for a specific timestep.
+    masterfield = loadfieldsPeter(nx,nz,Lx,Lz,fileext,gdadir); 
   else
     masterfield = loadfields(nx,nz,Lx,Lz,slice,gdadir);
   double *vx, *vy, *vz;
@@ -49,7 +51,7 @@ int main(int argc,char *argv[]){
   else dvy = 2*vymax/(Nvy-1);
   if(Nvz == 1) dvz =  0;
   else dvz = 2*vzmax/(Nvz-1);
-  
+  //set up IC arrays
   for(int i = 0; i< Nvx; i++){
     for(int j = 0; j< Nvy; j++){
       for(int k = 0; k<Nvz; k++){
@@ -60,7 +62,7 @@ int main(int argc,char *argv[]){
       }
     }
   }
-  for(int npos = 0; npos<Npoints; npos++){
+  for(int npos = 0; npos<Npoints; npos++){ //go by position
     char endfilename[512];
     *endfilename = '\0';
     strcat(endfilename,outdir);
@@ -69,8 +71,8 @@ int main(int argc,char *argv[]){
     sprintf(temp, "%sx%04.0fz%04.0frank%d.tsv",jobname,x0[npos],z0[npos],rank);
     strcat(endfilename,temp);
     FILE *endpointfile = fopen(endfilename,"w");
-#pragma omp parallel for schedule(dynamic)
-      for(int j = rank; j<(nICs); j+=numprocs){
+#pragma omp parallel for schedule(dynamic) //allow omp to dynamically parallelize most of the ICs.
+    for(int j = rank; j<(nICs); j+=numprocs){ //Assign ranks ICs on an alternating basis (hope close in velocity space implies close in load)
 	posvel IC;
 	IC.x = x0[npos];
 	IC.y = 0.0;
@@ -79,10 +81,12 @@ int main(int argc,char *argv[]){
 	IC.vy = vy[j];
 	IC.vz = vz[j];
 	if(DoTrack)
+	  //Only forward track for traces
 	  solveorbit(IC,masterfield);
-	posvel endpoint = solveorbitB(IC,masterfield);
+	posvel endpoint = solveorbitB(IC,masterfield); //creates mapping
         #pragma omp critical
 	{
+	  //Only allows one thread to write to the mapping file at a time.
 	  fprintf(endpointfile,  "%.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\n"\
 		  ,IC.x,IC.z,IC.vx,IC.vy,IC.vz,endpoint.x,endpoint.z,endpoint.vx,endpoint.vy,endpoint.vz);
 	}
@@ -90,8 +94,8 @@ int main(int argc,char *argv[]){
     fclose(endpointfile);
   }
   diff = clock() - start;
-  int sec = diff/CLOCKS_PER_SEC;
-  printf("Time: %d s\n", sec);
+  int sec = diff/CLOCKS_PER_SEC; //A poor measure of time
+  printf("Time: %d s\n", sec); 
   MPI_Finalize();
   return 0;
 }
@@ -113,6 +117,7 @@ int dxdt(double t, const double y[], double dydt[], void *params){
   dydt[4] = -fields.Ey + vx*fields.Bz - vz*fields.Bx;
   dydt[5] = -fields.Ez + vy*fields.Bx - vx*fields.By;
   return GSL_SUCCESS;
+  //straight up Lorentz force
 }
 
 int dxdtB(double t, const double y[], double dydt[], void *params){
@@ -132,11 +137,13 @@ int dxdtB(double t, const double y[], double dydt[], void *params){
   dydt[4] = -fields.Ey - vx*fields.Bz + vz*fields.Bx;
   dydt[5] = -fields.Ez - vy*fields.Bx + vx*fields.By;
   return GSL_SUCCESS;
+  //Lorentz force under time reversal.
 }
 
 
 int jacobian (double t, const double y[], double *dfdy, double dfdt[], void *params)
 {
+  //could be used with implicit method
   (void) (t);
   (void) (params);
   (void) (y);
@@ -149,6 +156,7 @@ int jacobian (double t, const double y[], double *dfdy, double dfdt[], void *par
 }
 
 posvel solveorbit(posvel IC, fieldgrid masterfield){
+  //Forward track
   char filestring[256];
   char buffer[1024];
   char *outstring = (char *) malloc(sizeof(char)*1024*nts);
@@ -158,7 +166,7 @@ posvel solveorbit(posvel IC, fieldgrid masterfield){
   char *out = concat(out1,filestring);
   FILE *fp = fopen(out,"w");
   gsl_odeiv2_system sys = {dxdt, jacobian, DIM, (void *)&masterfield};
-  gsl_odeiv2_driver * d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk8pd,1e-6, 1e-6, 0.0);
+  gsl_odeiv2_driver * d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk8pd,1e-6, 1e-6, 0.0); //Runge-Kutta 8 now, could be an issue
   double t = 0.0;
   double y[6] = {IC.x, IC.y, IC.z, IC.vx, IC.vy, IC.vz};
   sprintf(buffer, "%.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\n",y[0],y[1],y[2],y[3],y[4],y[5],t);
@@ -189,6 +197,7 @@ posvel solveorbit(posvel IC, fieldgrid masterfield){
 }
 
 posvel solveorbitB(posvel IC, fieldgrid masterfield){
+  //Back track
   char filestring[256];
   char buffer[1024];
   char *outstring = (char *) malloc(sizeof(char)*1024*nts);
@@ -200,7 +209,7 @@ posvel solveorbitB(posvel IC, fieldgrid masterfield){
   gsl_odeiv2_system sys = {dxdtB, jacobian, DIM, (void *)&masterfield};
   gsl_odeiv2_driver * d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk2,1e-6, 1e-6, 0.0);
   double t = 0.0;
-  double y[6] = {IC.x, IC.y, IC.z, -IC.vx, -IC.vy, -IC.vz};
+  double y[6] = {IC.x, IC.y, IC.z, -IC.vx, -IC.vy, -IC.vz}; //Time reversal
   if(DoTrack){
     sprintf(buffer, "%.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\t %.5f\n",y[0],y[1],y[2],-y[3],-y[4],-y[5],-t);
     strcat(outstring,buffer);
@@ -223,9 +232,10 @@ posvel solveorbitB(posvel IC, fieldgrid masterfield){
     }
   }
   posvel FC;
-  FC.x = y[0]; FC.y = y[1]; FC.z = y[2]; FC.vx = -y[3]; FC.vy = -y[4]; FC.vz = -y[5]; 
+  FC.x = y[0]; FC.y = y[1]; FC.z = y[2]; FC.vx = -y[3]; FC.vy = -y[4]; FC.vz = -y[5]; //time reversal
   gsl_odeiv2_driver_free(d);
   if(DoTrack)
+    //Since this can be run without the full track, needs a conditional
     fprintf(fp,"%s",outstring);
   fclose(fp);
   free(outstring);
@@ -234,6 +244,7 @@ posvel solveorbitB(posvel IC, fieldgrid masterfield){
 
 
 int initialize(){
+  //reads in the parameters desired. Not powerful now, as the file needs to follow this exact format.
   char buff[256];
   FILE * fp = fopen("orbit.cfg","r");
   fscanf(fp,"job-name %s\n",jobname);
@@ -281,6 +292,7 @@ int initialize(){
 }
 
 int outcheck(double x,double z){
+  //Return 1 if you've left the bounding box
   if((x>maxx)||(x<minx)||(z>maxz)||(z<minz)) return 1;
   return 0;
 }
